13a3be626a41096d46d2f5efa5b603b10956d9f7
pkikl <peter.kikl02@htl-salzburg.ac.at>
2022-12-07 15:05:00 +0100

    Added Unity File

---
+++ Assets/_MyGame/Scripts/HelloWorld2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextManager: MonoBehaviour
+{
+    [SerializeField] private Text inputText;
+    [SerializeField] private TextMeshProUGUI finalText;
+
+    public void LoadText()
+    {
+        finalText.text = inputText.text;
+    }
+}
